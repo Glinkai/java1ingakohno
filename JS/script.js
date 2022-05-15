2391b3546819a0b3a880c0e24c7540e4697c5caf
Glinkai <101972840+Glinkai@users.noreply.github.com>
2022-05-15 16:40:18 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,46 @@
+// 1)
+// console.log("Hello1");
+
+// 2)
+// let message = 'Hello world';
+// console.log(message);
+
+// message = 200;;
+// console.log(message);
+
+// 3) Типы данных
+// let str = "One";
+// let str2 = 'Two';
+// let str3 = `Another ${str}`;
+
+// console.log(str);
+// console.log(str2);
+// console.log(str3);
+
+// let bool = true;
+// let boolStr = "true";
+// console.log(bool);
+// console.log(typeof(boolStr), "boolStr");
+
+// let boolStrType = typeof(boolStr);
+
+// 4) Alert, prompt, confirm
+
+// alert("Hello world");
+
+// let userName = prompt("What is your name?", "User");
+// console.log(`Hello, ${userName}`);
+
+// let isBoss = confirm("Ты здесь главный?");
+// console.log(isBoss);
+
+// 5)
+let value = true;
+console.log(value);
+
+value = String(value);
+console.log(value);
+
+let math = "6" / "2";
+let mathNaN = "dmdkmdk" / "2";
+console.log(math, mathNaN);
